ed58991a7ad5a6bc51a26bf717e5e958ca83e05e
vaibhav <vaibhav.kum5789@gmail.com>
2025-04-14 17:59:01 +0530

    questions4 week2

---
+++ week2/MergekSortedLists.c
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode dummy;
+    struct ListNode* tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 && l2) {
+        if (l1->val < l2->val) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    tail->next = l1 ? l1 : l2;
+    return dummy.next;
+}
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+    if (listsSize == 0) return NULL;
+    if (listsSize == 1) return lists[0];
+
+    struct ListNode* result = lists[0];
+    for (int i = 1; i < listsSize; ++i) {
+        result = mergeTwoLists(result, lists[i]);
+    }
+
+    return result;
+}
